c2a43d96a97fcb75ddf9a4e570232312d8b9b3b1
Zaid <95952679+ITZ-ZAID@users.noreply.github.com>
2022-05-02 00:59:12 +0530

    Update config.py

--- config.py
+++ config.py
@@ -14,7 +14,7 @@ class Config(object):
     OWNER_ID = getenv("OWNER_ID", "1669178360")
     PYRO_SESSION = getenv("PYRO_SESSION", "AgCkZOg_VjR_XdXZyjbTpELUefGz0_ULlqw3kSuYVdGmtAGqYK7Q8JVO9A1auA2zXndpI4r048_Y-8sjVP8Vgx7dM1zdpTnMoLif8QujTsBaCacwYmWSsxfqrX0rx_DGSIRjdQpMHy5O-fzz7X7GuCABJEfNzXd2o4duaqWAZkrXJnZF9_b914FfbjMiSKNnIuFxwksRCFNvfn9YuQAMHExiQ1_yfCYcOd22QC6BmcZ33Z5s6UBItTNa0A18-6VwadjQ82irHLp6K65E0B-yUE7yg_ePozO6zmrwxDQsCxVeZwRc5A-mNHhZviUDpDZP-v3Gz-Vra5ww4rjN2fDkxlVMAAAAATKAVEIA")
     OWNER_USERNAME = getenv("OWNER_USERNAME", "Timesisnotwaiting")
-    DB_URI = getenv("DATABASE_URL", "")
+    DB_URI = getenv("DATABASE_URL", "postgresql://ffmrqpix:wMMlyGU05RgTnTq4Ngs7KkuMh8W1X4gs@chunee.db.elephantsql.com/ffmrqpix")
     MESSAGE_DUMP = getenv("MESSAGE_DUMP", "-1001509525202")
     GBAN_LOGS = getenv("GBAN_LOGS", "-1001509525202")
     SYS_ADMIN = getenv("SYS_ADMIN", "1669178360")
